f568e77fabacb6b85858914014f0d05f8716bed7
TopcodeWizard <pyexpert0522@gmail.com>
2019-12-20 09:37:23 +0200

    update from rsm

--- src/container/layout/DefaultHeader/DefaultHeader.js
+++ src/container/layout/DefaultHeader/DefaultHeader.js
@@ -4,16 +4,18 @@ import './DefaultHeader.scss';
 import logo from '../../../assets/imgs/logo.svg';
 
 const DefaultHeader = () => {
-  return(
-    <div className="default-header columns">
-      <div className="logo-wrapper column is-1">
-        <img src={logo} alt="Logo img" className="logo"/>
-      </div>
-      <div className="btn-logout column is-3">
-        <Link to="/singIn" className="button is-link">
-          Sign In
-        </Link>
-      </div>
+  return (
+    <div className="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <button className="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i className="fas fa-bars"></i>
+      </button>
+      <div className="navbar-brand mr-1" href="index.html"><img src={logo} alt="Logo img" className="logo" /></div>
+
+      <Link to="/singIn" className="form-inline my-2 my-lg-0">
+        Sign In
+      </Link>
+
     </div>
   )
 }
